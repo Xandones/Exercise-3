7b78f322deb51a3fba691fd8590ed4d1b6d7a0d5
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2023-09-28 21:45:30 -0300

    Add project files.

---
+++ YetAnotherMessage.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+	// Written by Alexandro H. Lopes in September 28th, 2023 
+	std::cout << "I'll" << std::endl;
+	std::cout << "learn" << std::endl;
+	std::cout << "to" << std::endl;
+	std::cout << "code" << std::endl;
+	std::cout << "in" << std::endl;
+	std::cout << "C++" << std::endl;
+	std::cout << "language" << std::endl;
+	std::cout << "I'll" << std::endl;
+	std::cout << "learn" << std::endl;
+	std::cout << "to" << std::endl;
+	std::cout << "code" << std::endl;
+	std::cout << "in" << std::endl;
+	std::cout << "C++" << std::endl;
+	std::cout << "language" << std::endl;
+	system("PAUSE");
+	return 0;
+	/*
+		This C++ writes a message splitted in parts through some lines.
+		It also has two comments as asked in the exercise.	
+	*/
+}
