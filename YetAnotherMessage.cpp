2c0248dba41c7a092fde8962152514c69793ba3a
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2024-02-15 11:23:15 -0300

    Fixed code

--- YetAnotherMessage.cpp
+++ YetAnotherMessage.cpp
@@ -3,14 +3,16 @@
 int main()
 {
 	// Written by Alexandro H. Lopes in September 28th, 2023 
-	std::cout << "I'll" << std::endl;
+	std::cout << "I" << std::endl;
+	std::cout << "will" << std::endl;
 	std::cout << "learn" << std::endl;
 	std::cout << "to" << std::endl;
 	std::cout << "code" << std::endl;
 	std::cout << "in" << std::endl;
 	std::cout << "C++" << std::endl;
 	std::cout << "language" << std::endl;
-	std::cout << "I'll" << std::endl;
+	std::cout << "I" << std::endl;
+	std::cout << "will" << std::endl;
 	std::cout << "learn" << std::endl;
 	std::cout << "to" << std::endl;
 	std::cout << "code" << std::endl;
@@ -20,7 +22,7 @@ int main()
 	system("PAUSE");
 	return 0;
 	/*
-		This C++ writes a message splitted in parts through some lines.
+		This C++ code writes a message splitted in parts through some lines.
 		It also has two comments as asked in the exercise.	
 	*/
 }
