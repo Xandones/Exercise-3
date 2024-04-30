691fdff3afbf981ea238a932c17039f34b6c2394
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2024-04-30 16:39:49 -0300

    Enhanced the exercise by doing it in a simpler way

--- YetAnotherMessage.cpp
+++ YetAnotherMessage.cpp
@@ -2,27 +2,13 @@
 
 int main()
 {
-	// Written by Alexandro H. Lopes in September 28th, 2023 
-	std::cout << "I" << std::endl;
-	std::cout << "will" << std::endl;
-	std::cout << "learn" << std::endl;
-	std::cout << "to" << std::endl;
-	std::cout << "code" << std::endl;
-	std::cout << "in" << std::endl;
-	std::cout << "C++" << std::endl;
-	std::cout << "language" << std::endl;
-	std::cout << "I" << std::endl;
-	std::cout << "will" << std::endl;
-	std::cout << "learn" << std::endl;
-	std::cout << "to" << std::endl;
-	std::cout << "code" << std::endl;
-	std::cout << "in" << std::endl;
-	std::cout << "C++" << std::endl;
-	std::cout << "language" << std::endl;
+	// Written by Alexandro H. Lopes in April 30th, 2024 
+	std::cout << "I\nwill\nlearn\nto\ncode\nin\nC++\nlanguage\n\nI\nwill\nlearn\nto\ncode\nin\nC++\nlanguage\n" << std::endl;
 	system("PAUSE");
 	return 0;
 	/*
 		This C++ code writes a message splitted in parts through some lines.
-		It also has two comments as asked in the exercise.	
+		It also has two comments as asked in the exercise.
+		It was enhanced in 2024.
 	*/
 }
